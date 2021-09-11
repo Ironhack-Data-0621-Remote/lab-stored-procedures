92950ee336a58b15946a9edfd3bb4aeab4dbf8b8
Sonia G Fominaya <76499685+Fominayasg@users.noreply.github.com>
2021-09-11 14:30:57 +0200

    Add files via upload

---
+++ Solutions.sql
@@ -0,0 +1,18 @@
+--
+-- Create a stored procedure that finds first name, last name, and emails of all the customers who rented `Action movies.
+-- 
+
+--
+-- Update the stored procedure in a such manner that it can take a string argument for the category name and return 
+-- the results for all customers that rented movie of that category/genre.
+--
+
+
+--
+-- Write a query to check the number of movies released in each movie category.
+--
+
+--
+-- Convert the query in to a stored procedure to filter only those categories that have movies released greater than a certain number. 
+-- Pass that number as an argument in the stored procedure.
+--
